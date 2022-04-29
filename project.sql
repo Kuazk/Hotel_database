b21aa71d54ea44727f3b1a44317daf3bafd0d5c4
Kuazk <allenz@M1-Mac-Allen.local>
2022-04-28 21:28:39 -0500

    update on sql

--- project.sql
+++ project.sql
@@ -1,12 +1,14 @@
+DROP DATABASE IF EXISTS hoteldb;
 
-DROP TABLE Floor;
+CREATE DATABASE hoteldb;
+USE hoteldb;
+
 CREATE TABLE Floor (
 	Floor_number INT,
     Ameneties VARCHAR(100),
     PRIMARY KEY (Floor_number)
 );
     
-DROP TABLE Room;
 CREATE TABLE Room (
 	Room_number INT,
     Room_type ENUM("King", "Queen", "Twin", "Standard Suite", "Presidential Suite"),
@@ -17,8 +19,7 @@ CREATE TABLE Room (
     FOREIGN KEY (Floor_number)
 	  REFERENCES Floor (Floor_number)
 );
-
-DROP TABLE Employee;    
+   
 CREATE TABLE Employee (
 	Employee_id INT AUTO_INCREMENT,
     Name VARCHAR(40),
@@ -30,7 +31,6 @@ CREATE TABLE Employee (
     PRIMARY KEY (Employee_id)
 );
 
-DROP TABLE ROOM_SERVICE_STAFF;
 CREATE TABLE ROOM_SERVICE_STAFF(
   Employee_id INT,
   Assigned_room INT,
@@ -43,7 +43,6 @@ CREATE TABLE ROOM_SERVICE_STAFF(
     REFERENCES EMPLOYEE (Employee_id)
 );
 
-DROP TABLE HOUSEKEEPING_STAFF;
 CREATE TABLE HOUSEKEEPING_STAFF (
   Employee_id INT,
   Current_floor INT,
@@ -56,30 +55,29 @@ CREATE TABLE HOUSEKEEPING_STAFF (
     REFERENCES EMPLOYEE (Employee_id)
 );
 
-DROP TABLE MANAGER;
 CREATE TABLE MANAGER (
+  Manager_id INT AUTO_INCREMENT,
   Employee_id INT,
-  Managed_employees VARCHAR(300)
 
-  PRIMARY KEY (Employee_id),
+  PRIMARY KEY (Manager_id),
   FOREIGN KEY (Employee_id)
-    REFERENCES Employee (Employee_id)
+    REFERENCES EMPLOYEE (Employee_id)
+
 );
 
-
-DROP TABLE Guest;    
+  
 CREATE TABLE Guest (
 	Guest_id INT AUTO_INCREMENT,
     Name VARCHAR(40),
     Date_of_birth DATE,
     Email VARCHAR(40),
     Address VARCHAR(40),
+    Room_number INT,
     PRIMARY KEY (Guest_id),
     FOREIGN KEY (Room_number)
 	    REFERENCES Room (Room_number)
 );
-
-DROP TABLE Requests;    
+   
 CREATE TABLE Requests (
 	Request_id INT AUTO_INCREMENT,
     Date_issued DATETIME,
@@ -93,18 +91,17 @@ CREATE TABLE Requests (
 	FOREIGN KEY (Employee_id)
 		REFERENCES Employee (Employee_id)
 );
-
-DROP TABLE Reservations;    
+  
 CREATE TABLE Reservations (
 	Booking_id INT AUTO_INCREMENT,
     Check_in DATE,
     Check_out  DATE,
+    Guest_id INT,
     PRIMARY KEY (Booking_id),
-    FOREIGN KEY (Name_of_Reserver)
+    FOREIGN KEY (Guest_id)
 		REFERENCES Guest (Guest_id)
 );
 
-DROP TABLE ARE_SERVED_BY;
 CREATE TABLE ARE_SERVED_BY (
   Employee_id INT,
   Room_number INT,
@@ -125,7 +122,7 @@ CREATE TABLE UPKEEPS(
     REFERENCES FLOOR (Floor_number),
   FOREIGN KEY (Employee_id)
     REFERENCES Employee (Employee_id)
-)
+);
 
 CREATE TABLE BOOKING(
   Guest_id INT,
@@ -139,15 +136,15 @@ CREATE TABLE BOOKING(
     REFERENCES GUEST (Guest_id),
   FOREIGN KEY (Booking_id)
     REFERENCES Reservations (Booking_id)
-)
+);
 
 CREATE TABLE ARE_MANAGED_BY(
   Employee_id INT,
   Floor_number INT,
 
-  PRIMARY KEY (Employee_id, Floor_number)
+  PRIMARY KEY (Employee_id, Floor_number),
   FOREIGN KEY (Floor_number)
     REFERENCES FLOOR (Floor_number),
   FOREIGN KEY (Employee_id)
     REFERENCES Employee (Employee_id)
-)
+);
